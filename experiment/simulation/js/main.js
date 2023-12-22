1ba6ec5e05d14e8cee352621830bcbb5860f370c
prakriti5dhang <prakritidhang@gmail.com>
2023-12-22 17:40:10 +0530

    phtitration

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,14 @@
 //Your JavaScript goes in here
+function start() {
+   
+    document.getElementById("step0").disabled = true;
+   
+    document.getElementById("step1").disabled = false;
+   
+    
+  }
+  
+  function restartexp() {
+    location.reload();
+  }
+  
